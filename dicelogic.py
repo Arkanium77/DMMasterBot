6b1f930b1ea15c5df1bf5ed06a7ef38119ff9744
Arkanium77 <Arkanium77@gmail.com>
2019-01-05 23:39:37 +0300

    Initial commit

---
+++ dicelogic.py
@@ -0,0 +1,118 @@
+from random import randrange as rnd
+import re
+
+diceTypes = [[2, "C","С"], 3, "F", 4, 8, 10, 12, [20, "D"], 100]
+
+NO = 'NO'
+
+
+def diceMaster(s):
+    s = s.split('d')
+    for i in range(len(s)):
+        s[i] = s[i].upper()
+    if (len(s) == 1):
+        s.append(s[0])
+        s[0] = 1
+    print(s)
+    if (len(s) != 2):
+        return NO
+    dices = diceRule(s[1])
+    print("dices -> ", dices)
+    type = typeRule(dices[0])
+    print("type -> ", type)
+    if type == NO:
+        return NO
+    if (type == "F"):
+        throws = throw(type, 4)
+        t = 0
+        for i in throws:
+            t += i
+        if (len(dices) > 1):
+            for j in dices[1:]:
+                t += int(j)
+        return [t]
+    quant = quantRule(s[0])
+    print("quant -> ", quant)
+    throws = throw(type, quant)
+    print("throws -> ", throws)
+
+    if len(dices) > 1 and (type != 2 or type != "2"):
+        for i in range(len(throws)):
+            for j in dices[1:]:
+                throws[i] += int(j)
+
+    elif type == 2 or type == "2":
+        for i in range(len(throws)):
+            if throws[i] == 0:
+                throws[i] = "tails"
+            else:
+                throws[i] = "heads"
+    print("throws+ -> ", throws)
+    return throws
+
+
+def diceRule(s):
+    dices = s.replace("-", "+-")
+    dices = dices.split("+")
+    return dices
+
+
+def typeRule(s):
+    print("TypeRule: " + s)
+    if (s == "F"):
+        return s
+    try:
+        s = int(s)
+    except:
+        pass
+    type = NO
+    for i in diceTypes:
+        print(i, "  ->  ", s)
+        if i == s:
+            type = i
+            print("+")
+            return type
+        else:
+            try:
+                print(inList(i, s))
+            except Exception:
+                continue
+            if inList(i, s):
+                type = i[0]
+                print("+")
+                return type
+
+        print("next")
+    return type
+
+
+def inList(list, el):
+    for i in list:
+        if i == el:
+            return True
+    return False
+
+
+def quantRule(s):
+    if (len(s) == 0):
+        return 1
+    if (int(s) > 0):
+        return int(s)
+    return NO
+
+
+def throw(type, quant):
+    if type == "F":
+        a = []
+        for i in range(0, 4):
+            a.append(rnd(-1, 2))
+        return a
+    if type == "2" or type==2:
+        a = []
+        for i in range(0, int(quant)):
+            a.append(rnd(0, type))
+        return a
+    a = []
+    for i in range(0, int(quant)):
+        a.append(rnd(1, type + 1))
+    return a
---
+++ main.py
@@ -0,0 +1,85 @@
+import telebot
+import os
+from telebot import apihelper
+from dicelogic import diceMaster
+import time
+import re
+
+token = os.getenv("TOKEN")
+bot = telebot.TeleBot(token)
+
+diceR = "[0-9]*d(2|C|c|3|F|4|6|8|10|12|20|D|d|100)[0-9^(+\-)]*"
+
+
+@bot.message_handler(regexp=diceR)
+def handle_message(message):
+
+    s = message.text
+    tst = re.sub(diceR, "!", s)
+    if (tst == "!"):
+        a = diceMaster(s)
+        # print(message)
+        bot.reply_to(message, messBuilder(message, a))
+    pass
+
+def messBuilder(message, a):
+    s = ""
+    str1 = message.from_user.username
+    str1 += "  throws:\n"
+    s += str1
+    for i in a:
+        s += str(i) + "     "
+    # s+="\n"
+    return s
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    print(message)
+    str = ""
+    if (message.from_user.language_code == "ru"):
+        str += """
+		Привет!\n
+		Этот бот создан для всех любителей ролевых игр. Он умеет бросать кости. Запросы к боту следует отправлять в формате:\n
+		1d4\n
+		d20\n
+		5d8+4-2\n
+		\n
+		Бот поддерживает следующие типы костей:\n
+		d2 или dC - монетка (Heads\Орёл = 1, Tails\Решка = 0)\n
+		dF - FUDGE Dice. Происходит бросок сразу 4х дайсов\n
+		d4, d8, d10, d12, d20, d100\n
+		"""
+    else:
+        str += """
+		Hello!\n
+		This bot is created for all fans of role-playing games. He can throw dices. Requests to the bot should be sent in the following format:\n
+		1d4\n
+		d20\n
+		5d8 + 4-2\n
+		\n
+		The bot supports the following dice types:\n
+		d2 or dC - coin (Heads = 1, Tails = 0)\n
+		dF - FUDGE Dice. 4 dice throws right away\n
+		d4, d8, d10, d12, d20, d100\n
+		"""
+    bot.reply_to(message, str)
+
+
+@bot.message_handler(func=lambda m: True)
+def echo_all(message):
+    print(message.text)
+    if re.search(r"((ужасы войны)|(ужасывойны))", message.text, re.MULTILINE | re.IGNORECASE):
+        bot.reply_to(message, "Ужасы войны, ужасы войны, ужасы войны...")
+    pass
+
+
+apihelper.proxy = {'https': 'https://67.205.146.54:80'}
+# apihelper.proxy = {'https': 'socks5://swcbbabh:aYEbh6q5gQ@bb8.vivalaresistance.info:3306'}
+
+if __name__ == '__main__':
+    while True:
+        try:
+            bot.polling(none_stop=True)
+        except Exception as e:
+            print("BOOM")
+            time.sleep(3)
