e714a21455cf343ee7980d3e757d2393bda26242
Arkanium77 <Arkanium77@gmail.com>
2019-01-14 18:39:52 +0300

    otkat

--- main.py
+++ main.py
@@ -118,13 +118,8 @@ def echo_all(message):
     pass
 
 
-initstat=False
 def yaSearch(message):
-    yaid=166131532
-    if not initstat:
-        bot.send_message(yaid,"/start")
-        bot.send_message(yaid, "москва")
-    bot.send_message(yaid,"Кот")
+    pass
 
 # apihelper.proxy = {'https': 'https://67.205.146.54:80'}
 # apihelper.proxy = {'https': 'socks5://swcbbabh:aYEbh6q5gQ@bb8.vivalaresistance.info:3306'}
